15d2bde12e96d39d3b219c897a855706c75788dd
David Marrs <david@marrsbytes.com>
2020-05-11 18:11:47 +0100

    Convert appState into globals

--- sawtooth.c
+++ sawtooth.c
@@ -7,26 +7,32 @@
 #include <jack/jack.h>
 #include <xcb/xcb.h>
 
-#define die_if(pred, appState, action) if (pred) { (action); shutdown_app(appState, EXIT_FAILURE); }
+#define die_if(pred, action) if (pred) { (action); shutdown_app(EXIT_FAILURE); }
 
-typedef struct {
+typedef struct JackState {
     jack_port_t *leftPort, *rightPort;
-    jack_client_t *jackClient;
-    xcb_connection_t *xcbConnection;
-    xcb_window_t xcbWindow;
-    xcb_gcontext_t xcbFgContext;
-    xcb_gcontext_t xcbBgContext;
-} AppState;
+    jack_client_t *client;
+} JackState;
+
+typedef struct XcbState {
+    xcb_connection_t *connection;
+    xcb_window_t window;
+    xcb_gcontext_t fgContext;
+    xcb_gcontext_t bgContext;
+} XcbState;
+
+
+JackState jackState;
+XcbState xcbState;
 
 int isShuttingDown = 0;
 
 int process(jack_nframes_t nframes, void *arg)
 {
-    AppState *appState = (AppState *)arg;
     jack_default_audio_sample_t *leftChannel, *rightChannel;
 
-    leftChannel = (jack_default_audio_sample_t *)jack_port_get_buffer(appState->leftPort, nframes);
-    rightChannel = (jack_default_audio_sample_t *)jack_port_get_buffer(appState->rightPort, nframes);
+    leftChannel = (jack_default_audio_sample_t *)jack_port_get_buffer(jackState.leftPort, nframes);
+    rightChannel = (jack_default_audio_sample_t *)jack_port_get_buffer(jackState.rightPort, nframes);
 
     FILE * pFile;
     pFile = fopen ("log","wa");
@@ -44,10 +50,10 @@ int process(jack_nframes_t nframes, void *arg)
         line[1].x = i;
         line[1].y = 10 + 10*val;
 
-        xcb_poly_line (appState->xcbConnection,
+        xcb_poly_line (xcbState.connection,
                        XCB_COORD_MODE_ORIGIN,
-                       appState->xcbWindow,
-                       appState->xcbFgContext,
+                       xcbState.window,
+                       xcbState.fgContext,
                        2,
                        line);
 
@@ -56,22 +62,20 @@ int process(jack_nframes_t nframes, void *arg)
         line[1].x = i;
         line[1].y = 255;
 
-        xcb_poly_line (appState->xcbConnection,
+        xcb_poly_line (xcbState.connection,
                        XCB_COORD_MODE_ORIGIN,
-                       appState->xcbWindow,
-                       appState->xcbBgContext,
+                       xcbState.window,
+                       xcbState.bgContext,
                        2,
                        line);
     }
-    xcb_flush (appState->xcbConnection);
-    fclose (pFile);
+    xcb_flush (xcbState.connection);
     return 0;
 }
 
 void jack_shutdown (void *arg)
 {
-    AppState *appState = (AppState *)arg;
-    appState->jackClient = NULL;
+    jackState.client = NULL;
 }
 
 void sigterm_action(int signum)
@@ -79,13 +83,13 @@ void sigterm_action(int signum)
     isShuttingDown = 1;
 }
 
-void shutdown_app(AppState *appState, int exitStatus)
+void shutdown_app(int exitStatus)
 {
-    if (NULL != appState->xcbConnection) {
-        xcb_disconnect (appState->xcbConnection);
+    if (NULL != xcbState.connection) {
+        xcb_disconnect (xcbState.connection);
     }
-    if (NULL != appState->jackClient) {
-        jack_client_close (appState->jackClient);
+    if (NULL != jackState.client) {
+        jack_client_close(jackState.client);
     }
     exit(exitStatus);
 }
@@ -94,31 +98,31 @@ int main()
 {
     AppState appState;
     /* Open the connection to the X server */
-    appState.xcbConnection = xcb_connect (NULL, NULL);
+    xcbState.connection = xcb_connect (NULL, NULL);
     uint32_t xcbvals[2];
 
 
     /* Get the first screen */
-    const xcb_setup_t      *setup  = xcb_get_setup(appState.xcbConnection);
+    const xcb_setup_t      *setup  = xcb_get_setup(xcbState.connection);
     xcb_screen_iterator_t   iter   = xcb_setup_roots_iterator (setup);
     xcb_screen_t           *screen = iter.data;
 
-    appState.xcbFgContext = xcb_generate_id(appState.xcbConnection);
+    xcbState.fgContext = xcb_generate_id(xcbState.connection);
     xcbvals[0]  = screen->white_pixel;
     xcbvals[1] = 0;
 
-    xcb_create_gc (appState.xcbConnection,
-                   appState.xcbFgContext,
+    xcb_create_gc (xcbState.connection,
+                   xcbState.fgContext,
                    screen->root,
                    XCB_GC_FOREGROUND | XCB_GC_GRAPHICS_EXPOSURES,
                    xcbvals);
 
-    appState.xcbBgContext = xcb_generate_id(appState.xcbConnection);
+    xcbState.bgContext = xcb_generate_id(xcbState.connection);
     xcbvals[0]  = screen->black_pixel;
     xcbvals[1] = 0;
 
-    xcb_create_gc (appState.xcbConnection,
-                   appState.xcbBgContext,
+    xcb_create_gc (xcbState.connection,
+                   xcbState.bgContext,
                    screen->root,
                    XCB_GC_FOREGROUND | XCB_GC_GRAPHICS_EXPOSURES,
                    xcbvals);
@@ -127,10 +131,10 @@ int main()
     xcbvals[1] = XCB_EVENT_MASK_EXPOSURE;
 
     /* Create the window */
-    appState.xcbWindow = xcb_generate_id (appState.xcbConnection);
-    xcb_create_window (appState.xcbConnection,        /* Connection          */
+    xcbState.window = xcb_generate_id (xcbState.connection);
+    xcb_create_window (xcbState.connection,        /* Connection          */
                        XCB_COPY_FROM_PARENT,          /* depth (same as root)*/
-                       appState.xcbWindow,            /* window Id           */
+                       xcbState.window,            /* window Id           */
                        screen->root,                  /* parent window       */
                        0, 0,                          /* x, y                */
                        256, 256,                      /* width, height       */
@@ -142,13 +146,13 @@ int main()
 
 
     /* Map the window on the screen */
-    xcb_map_window (appState.xcbConnection, appState.xcbWindow);
+    xcb_map_window (xcbState.connection, xcbState.window);
 
     // Create drawing context
 
 
     /* Make sure commands are sent before we pause so that the window gets shown */
-    xcb_flush (appState.xcbConnection);
+    xcb_flush (xcbState.connection);
 
 
 
@@ -158,10 +162,10 @@ int main()
 	jack_status_t jcStatus;
 
     
-	appState.jackClient = jack_client_open(jcName, jcOptions, &jcStatus, jcServerName);
-    jack_client_t *jackClient = appState.jackClient;
+	jackState.client = jack_client_open(jcName, jcOptions, &jcStatus, jcServerName);
+    jack_client_t *jackClient = jackState.client;
 
-    die_if(NULL == jackClient, &appState, {
+    die_if(NULL == jackClient, {
 		fprintf (stderr, "jack_client_open() failed, "
 			 "status = 0x%2.0x\n", jcStatus);
 		if (jcStatus & JackServerFailed) {
@@ -176,33 +180,33 @@ int main()
 		fprintf (stderr, "unique name `%s' assigned\n", jcName);
 	}
 
-    jack_set_process_callback(jackClient, process, &appState);
+    jack_set_process_callback(jackClient, process, &jackState);
 
-    appState.leftPort = jack_port_register(jackClient,
+    jackState.leftPort = jack_port_register(jackClient,
                                              "leftChannel",
 					                         JACK_DEFAULT_AUDIO_TYPE,
 					                         JackPortIsOutput, 0);
 
-    appState.rightPort = jack_port_register(jackClient,
+    jackState.rightPort = jack_port_register(jackClient,
                                               "rightChannel",
 					                          JACK_DEFAULT_AUDIO_TYPE,
 					                          JackPortIsOutput, 0);
 
-    die_if(NULL == appState.leftPort || NULL == appState.rightPort, &appState,
+    die_if(NULL == jackState.leftPort || NULL == jackState.rightPort,
             fprintf(stderr, "Could not connect to Jack")
     );
 
-    die_if(jack_activate(jackClient), &appState,
+    die_if(jack_activate(jackClient),
             fprintf(stderr, "Could not activate jack client"));
 
 	const char **ports = jack_get_ports(jackClient, NULL, NULL,
 				JackPortIsPhysical|JackPortIsInput);
-	die_if(ports == NULL, &appState, fprintf(stderr, "no physical capture ports\n"));
+	die_if(ports == NULL, fprintf(stderr, "no physical capture ports\n"));
 
-	if (jack_connect(jackClient, jack_port_name(appState.leftPort), ports[1])) {
+	if (jack_connect(jackClient, jack_port_name(jackState.leftPort), ports[1])) {
 		fprintf (stderr, "cannot connect left port\n");
 	}
-	if (jack_connect(jackClient, jack_port_name(appState.rightPort), ports[0])) {
+	if (jack_connect(jackClient, jack_port_name(jackState.rightPort), ports[0])) {
 		fprintf (stderr, "cannot connect right port\n");
 	}
 
@@ -214,12 +218,12 @@ int main()
 
     for (;;) {
         sleep(1);
-        if (isShuttingDown) shutdown_app(&appState, EXIT_SUCCESS);
+        if (isShuttingDown) shutdown_app(EXIT_SUCCESS);
     }
 
     
 
-    jack_on_shutdown(jackClient, jack_shutdown, &appState);
+    jack_on_shutdown(jackClient, jack_shutdown, &jackState);
 
 
     return 0;
